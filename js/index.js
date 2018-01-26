402f0b1c8e8797b03ab7f8c586ba59f76667d4b0
chris <dwatow@gmail.com>
2018-01-26 13:02:53 +0800

    fix router

--- js/index.js
+++ js/index.js
@@ -2,7 +2,3 @@ var router = new Router();
 router.add('index', () => index.checked = true)
 router.add('active', () => active.checked = true)
 router.add('feedback', () => feedback.checked = true)
-
-
-router.go('active')
-router.go('feedback')
