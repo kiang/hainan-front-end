27d6f05b89c31459c2e3ae40170b1b8cfe61ae10
chris <dwatow@gmail.com>
2018-01-26 14:41:05 +0800

    modify map list

--- js/index.js
+++ js/index.js
@@ -2,3 +2,7 @@ var router = new Router();
 router.add('index', () => index.checked = true)
 router.add('active', () => active.checked = true)
 router.add('feedback', () => feedback.checked = true)
+
+
+const listing = document.querySelector('#listing');
+listing.addEventListener('click', () => mapsList.checked = false)
